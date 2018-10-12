2516a51c67b836c14ce19030283b4f7ca13ff835
Sonic Keung <skeung3@my.bcit.ca>
2018-10-12 16:18:40 -0700

    clean up any duplicating code

--- canine.hpp
+++ canine.hpp
@@ -5,22 +5,14 @@ class Canine : public Animal {
 
 public:
     Canine();
-
     Canine(int age1, double x1, double y1);
-
     virtual void moves(double x, double y);
-
-    // destructor
     virtual ~Canine();
-
-    // copy constructor
     Canine(const Canine &a);
-
     void sleep() override;
     void eat() override;
     void hunt();
 
-    // insertion operator
     friend std::ostream& operator<<(std::ostream& os, const Canine&);
 
 };
